fde9eba78c4e9b02d11efaedd23c2945a4c26d99
Milk Ory <youranpublic@gmail.com>
2022-02-03 17:35:38 +0800

    :sparkles: subtitle_generator

---
+++ subtitle_generator.py
@@ -0,0 +1,58 @@
+import sys
+import getopt
+
+
+def string_insert(ori, index, text):
+    str_list = list(ori)
+    str_list.insert(index, text)
+    return ''.join(str_list)
+
+
+def help(code=0):
+    print('subtitle_generator.py -s1 -e0 -btimer --text "SAMPLE TEXT"')
+    exit(code)
+
+
+startTick = 1
+endTick = 0
+text = ''
+step = 2
+
+entry = 't0'
+scoreboard = 'm.'
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 's:e:b:h', ['text=', 'step='])
+except getopt.getopt.GetoptError:
+    help(-1)
+
+for opt, arg in opts:
+    if opt == '-s':
+        startTick = int(arg)
+    elif opt == '-e':
+        entry = arg
+    elif opt == '-b':
+        scoreboard = arg
+    elif opt == '--text':
+        text = arg
+    elif opt == '--step':
+        step = int(arg)
+    elif opt == '-h':
+        help()
+
+if text == '':
+    print('Option "--text" is necessary.')
+    sys.exit(-2)
+
+base = 'execute if score %s %s matches {tick} run title @a subtitle "{text}"\n' % (
+    entry, scoreboard)
+
+build_text = ''
+
+with open('output.mcfunction', 'w', encoding='utf-8') as output:
+    for char in text:
+        build_text += char
+        output.write(base.format(tick=startTick,
+                                 text=string_insert(build_text, len(build_text) - 1, '\u00a77')))
+        startTick += step
+    output.write(base.format(tick=startTick, text=text))
